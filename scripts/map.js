f07134a0db3a1b66f7c55ebe94dfa6ff3fb3d38b
cjacks0413 <chjackson93@gmail.com>
2014-10-13 15:49:09 -0400

    add labels and customcss, filter based on toptype

--- scripts/map.js
+++ scripts/map.js
@@ -1,27 +1,28 @@
-//------------------------------------------
-// BASIC MAP SETUP
-//------------------------------------------
+/*----------------------------------------
+ * BASIC MAP SETUP 
+ *----------------------------------------*/ 
+$j = jQuery; 
 L.mapbox.accessToken = 'pk.eyJ1IjoiY2phY2tzMDQiLCJhIjoiVFNPTXNrOCJ9.k6TnctaSxIcFQJWZFg0CBA';
 var baseLayer = L.mapbox.tileLayer('cjacks04.jij42jel');
 var map = new L.Map('map', {
 	            center: new L.LatLng( 34, 34 ),
-				zoom: 6,
+				zoom: 5,
 				layers: [baseLayer],
 				minZoom: 3,
 				maxZoom: 11
 });
 
-//----------------------------------------
-//ROUTES AND SITES LAYERS
-//----------------------------------------
+/*----------------------------------------
+ * ROUTES AND SITES LAYERS
+ *----------------------------------------*/ 
 var routeLayer = L.layerGroup(); 
-routeLayer = L.geoJson(allRoutes).addTo(map); 
+	routeLayer.addLayer(L.geoJson(allRoutes)); 
 
-$j = jQuery; 
 
-//-----------------------------------------
-//FREQUENCY LIST FOR TOPTYPES
-//-----------------------------------------
+
+/*-----------------------------------------
+ * FREQUENCY LIST/DICTIONARY FOR TOPTYPES
+ *-----------------------------------------*/
 
 var sites = {}; 
 	$j.each( places.data, function(_id, _site) {
@@ -33,15 +34,15 @@ var sites = {};
 		}
 	}); 
  
-//------------------------------------------------------------
-//  Build a layer for each type
-//------------------------------------------------------------
+/*------------------------------------------
+ * DIFFERENTIATE BETWEEN TOPTYPES
+ *------------------------------------------*/ 
 var layerStyles = {
-	"capitals" : { color: '#6F9690', fillColor: '#6F9690', radius: 15, opacity: 0, fillOpacity: 1 }, 
-	"metropoles" : {color: '#6F9690', fillColor: '#6F9690', radius: 10, opacity: 0, fillOpacity: 1}, 
+	"capitals" : { color: '#6F9690', fillColor: '#6F9690', radius: 8, opacity: 0, fillOpacity: .8 }, 
+	"metropoles" : {color: '#6F9690', fillColor: '#6F9690', radius: 15, opacity: 0, fillOpacity: 1}, 
 	"towns" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 1 }, 
 	"villages" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 1 },
-	"temp" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 1 } 
+	"temp" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 0 } 
 }
 
 
@@ -52,23 +53,39 @@ var layerStyles = {
 // 	if (style != undefined) {
 // 		addMarkerForTopType(sites[key], style); 
 // 	}
-// }; 
+// };
 
-/* TEMP FIX */ 
-addMarkerForTopType(sites["metropoles"], layerStyles["metropoles"]);
-addMarkerForTopType(sites["capitals"], layerStyles["capitals"]); 
 
 var sitesLayer = L.layerGroup(); 
-function addMarkerForTopType(topTypes, style) {
-	$j.each(topTypes, function (id, place) {
-		var marker = L.circleMarker([place.lat, place.lon], style).on('mouseover', function() {
-			this.bindPopup(place.arTitle)
-				.openPopup(); 
-		}).on('mouseout', function() {
-			this.closePopup();
-		});  
-		sitesLayer.addLayer(marker); 
+var staticSitesLayer = L.layerGroup(); 
+
+/* TEMP FIX */ 
+//addMarkerForTopType(sites["metropoles"], layerStyles["metropoles"]);
+//addMarkerForTopType(sites["capitals"], layerStyles["capitals"]); 
+
+addLayerForTopType(sites["capitals"], sitesLayer, layerStyles["capitals"], false, "leaflet-label-city"); 
+addLayerForTopType(sites["metropoles"], sitesLayer, layerStyles["metropoles"], true, "leaflet-label-city"); 
+addLayerForTopType(sites["sea"], sitesLayer, layerStyles["temp"], true, "leaflet-label-waters"); 
+addLayerForTopType(sites["towns"], sitesLayer, layerStyles["towns"], false, "leaflet-label-city"); 
+
+function addLayerForTopType(topTypes, layer, style, noHide, labelClass) {
+	$j.each(topTypes, function(id, place) {
+		var marker = L.circleMarker([place.lat, place.lon], style)
+					  .bindLabel(place.arTitle, {
+					  	noHide: noHide,
+					    className: labelClass
+					  })
+					  .on('click', function() {
+					  	this.bindPopup(createPopup(place)).openPopup(); 
+					  });
+		layer.addLayer(marker); 
 	}); 
+}
+
+function createPopup(place) {
+	return '<center><span class="arabic">' + place.arTitle + 
+	'</span><br><span class="english">' + place.translitTitle + 
+	'</span><br><b>Check in:</b> <a href="http://pleiades.stoa.org/search?SearchableText='+place.translitSimpleTitle+'" target="_blank">Pleiades</a>; <a href="https://en.wikipedia.org/wiki/Special:Search/'+place.translitSimpleTitle+'" target="_blank">Wikipedia</a></center>'
 }
 
 var baseMaps = {
