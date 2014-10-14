5a3307c2d61c5e97ac9b40b82be1f3cbdedceb1d
cjacks0413 <chjackson93@gmail.com>
2014-10-14 12:59:04 -0400

    adding comments

--- scripts/map.js
+++ scripts/map.js
@@ -2,6 +2,7 @@
  * BASIC MAP SETUP 
  *----------------------------------------*/ 
 $j = jQuery; 
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiY2phY2tzMDQiLCJhIjoiVFNPTXNrOCJ9.k6TnctaSxIcFQJWZFg0CBA';
 var baseLayer = L.mapbox.tileLayer('cjacks04.jij42jel');
 var map = new L.Map('map', {
@@ -16,58 +17,59 @@ var map = new L.Map('map', {
  * ROUTES AND SITES LAYERS
  *----------------------------------------*/ 
 var routeLayer = L.layerGroup(); 
-	routeLayer.addLayer(L.geoJson(allRoutes)); 
 
-
+routeLayer.addLayer(L.geoJson(allRoutes)); 
 
 /*-----------------------------------------
  * FREQUENCY LIST/DICTIONARY FOR TOPTYPES
  *-----------------------------------------*/
 
 var sites = {}; 
-	$j.each( places.data, function(_id, _site) {
-		if (sites[_site.topType] == undefined) {
-			sites[_site.topType] = [];
-			sites[_site.topType].push(_site); 
-		} else {
-			sites[_site.topType].push(_site); 
-		}
-	}); 
- 
-/*------------------------------------------
+
+$j.each( places.data, function(_id, _site) {
+	if (sites[_site.topType] == undefined) {
+		sites[_site.topType] = [];
+		sites[_site.topType].push(_site); 
+	} else {
+		sites[_site.topType].push(_site); 
+	}
+}); 
+
+/*-----------------------------------------------------
  * DIFFERENTIATE BETWEEN TOPTYPES
- *------------------------------------------*/ 
+ * layerStyles: handles the formatting of the different
+ * 	            circleMarkers. 
+ * sitesLayer : currently, all sites are added to the siteLayer
+ * 	 			but any layer can be passed in. 
+ * addLayerForTopType( topTypes // some set of types, accessed
+ *							       using sites[TOPTYPE]
+ *					   layer // layer to add to map
+ *					   styles // styling for circle marker)
+ *					   noHide // true if label should always
+ *								be present on the map
+ *				  	   labelClass // styling for the label 
+ *-----------------------------------------------------*/ 
+
 var layerStyles = {
 	"capitals" : { color: '#6F9690', fillColor: '#6F9690', radius: 8, opacity: 0, fillOpacity: .8 }, 
 	"metropoles" : {color: '#6F9690', fillColor: '#6F9690', radius: 15, opacity: 0, fillOpacity: 1}, 
 	"towns" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 1 }, 
 	"villages" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 1 },
+	//add in waystations, smallest, only at certain zoom level 
 	"temp" : { color: '#6F9690', fillColor: '#6F9690', radius: 4, opacity: 0, fillOpacity: 0 } 
 }
 
 
-/*some variation on this loop...*/ 
-//var style; 
-// for (var key in sites) {
-// 	style = layerStyles[key]; 
-// 	if (style != undefined) {
-// 		addMarkerForTopType(sites[key], style); 
-// 	}
-// };
-
-
 var sitesLayer = L.layerGroup(); 
-var staticSitesLayer = L.layerGroup(); 
 
-/* TEMP FIX */ 
-//addMarkerForTopType(sites["metropoles"], layerStyles["metropoles"]);
-//addMarkerForTopType(sites["capitals"], layerStyles["capitals"]); 
 
 addLayerForTopType(sites["capitals"], sitesLayer, layerStyles["capitals"], false, "leaflet-label-city"); 
 addLayerForTopType(sites["metropoles"], sitesLayer, layerStyles["metropoles"], true, "leaflet-label-city"); 
 addLayerForTopType(sites["sea"], sitesLayer, layerStyles["temp"], true, "leaflet-label-waters"); 
 addLayerForTopType(sites["towns"], sitesLayer, layerStyles["towns"], false, "leaflet-label-city"); 
 
+//sea, dark blue
+//add in island, diff color
 function addLayerForTopType(topTypes, layer, style, noHide, labelClass) {
 	$j.each(topTypes, function(id, place) {
 		var marker = L.circleMarker([place.lat, place.lon], style)
